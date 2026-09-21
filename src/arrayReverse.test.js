2fe467fd8b84d81b91b5a9a149dd567135b13d19
Olga Berezhna <57196165+oberezhnay@users.noreply.github.com>
2026-09-21 12:33:23 +0300

    Add tests for arrayReverse function

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -7,12 +7,28 @@ describe(`Function 'arrayReverse':`, () => {
     expect(arrayReverse).toBeInstanceOf(Function);
   });
 
-  it(`should return an array`, () => {});
+  it(`should return an array`, () => {
+    const result = arrayReverse(['hello', 'world']);
+    expect(result).toBeInstanceOf(Array);
+  });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([''])).toEqual(['']);
   });
 
-  // write more tests here
+  it(`should return a reversed string`, () => {
+    expect(arrayReverse(['Hell0'])).toEqual(['0lleH']);
+  });
+
+  it(`should return reversed strings`, () => {
+    const result = arrayReverse(['Mate', 'Academy']);
+    expect(result).toEqual(['ymed', 'aceteM']);
+  });
+
+  it(`should return reversed strings right length`, () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!']))
+    .toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
+
 });
